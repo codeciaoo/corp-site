16fbfd1bbd0e05745788db5ace16450a2567c71e
Daiki Kawagishi <daikikawagishi@gmail.com>
2025-01-18 22:09:31 +0000

    seo crawler用のrobots.txt追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [
-    react(),
-    tailwind(),
-  ],
-}); 
+  site: "https://code-ciao.com",
+  integrations: [react(), tailwind(), sitemap()],
+});
