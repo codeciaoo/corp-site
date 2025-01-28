49f94991faadf93cfa88804454ffd95a6425fc73
Daiki Kawagishi <daikikawagishi@gmail.com>
2025-01-28 23:09:43 +0000

    .astro fileをgitから削除

--- .astro/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_eNUel from "./mermaid.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2F2024-01-15_%E3%83%9B%E3%83%BC%E3%83%A0%E3%83%9A%E3%83%BC%E3%82%B8%E3%81%AB%E8%BC%89%E3%81%9B%E3%82%8B%E3%81%9F%E3%82%81%E3%81%AE%E8%B3%87%E6%96%99.md";
-export default new Map([["./mermaid.png?astroContentImageFlag=&importer=src%2Fcontent%2Fprojects%2F2024-01-15_%E3%83%9B%E3%83%BC%E3%83%A0%E3%83%9A%E3%83%BC%E3%82%B8%E3%81%AB%E8%BC%89%E3%81%9B%E3%82%8B%E3%81%9F%E3%82%81%E3%81%AE%E8%B3%87%E6%96%99.md", __ASTRO_IMAGE_IMPORT_eNUel]]);
-		
